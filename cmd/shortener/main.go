d501e7773bd060bdc18280d5f1083c5f96130e2e
salakhutdinov.m <marat.salakhutdinov@atme.com>
2024-09-27 04:42:41 +0300

    added encoding decoding

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/jayjaytrn/URLShortener/config"
 	"github.com/jayjaytrn/URLShortener/internal/handlers"
+	"github.com/jayjaytrn/URLShortener/internal/middleware"
 	"github.com/jayjaytrn/URLShortener/logging"
 	"net/http"
 )
@@ -16,9 +17,39 @@ func main() {
 	defer sugar.Sync()
 
 	r := chi.NewRouter()
-	r.Post(`/`, handlers.WithLogging(handlers.URLWaiter, sugar))
-	r.Post(`/api/shorten`, handlers.WithLogging(handlers.Shorten, sugar))
-	r.Get(`/{id}`, handlers.WithLogging(handlers.URLReturner, sugar))
+	r.Post(`/`,
+		func(w http.ResponseWriter, r *http.Request) {
+			middleware.Conveyor(
+				http.HandlerFunc(handlers.URLWaiter),
+				sugar,
+				middleware.WithLogging,
+				middleware.WriteWithCompression,
+				middleware.ReadWithCompression,
+			).ServeHTTP(w, r)
+		},
+	)
+	r.Post(`/api/shorten`,
+		func(w http.ResponseWriter, r *http.Request) {
+			middleware.Conveyor(
+				http.HandlerFunc(handlers.Shorten),
+				sugar,
+				middleware.WithLogging,
+				middleware.WriteWithCompression,
+				middleware.ReadWithCompression,
+			).ServeHTTP(w, r)
+		},
+	)
+
+	r.Get(`/{id}`,
+		func(w http.ResponseWriter, r *http.Request) {
+			middleware.Conveyor(
+				http.HandlerFunc(handlers.Shorten),
+				sugar,
+				middleware.WithLogging,
+				middleware.WriteWithCompression,
+			).ServeHTTP(w, r)
+		},
+	)
 
 	err := http.ListenAndServe(config.Config.ServerAddress, r)
 	if err != nil {
---
+++ internal/middleware/middleware.go
@@ -0,0 +1,137 @@
+package middleware
+
+import (
+	"compress/gzip"
+	"go.uber.org/zap"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type (
+	loggingResponseWriter struct {
+		http.ResponseWriter
+		responseData *responseData
+	}
+
+	responseData struct {
+		status int
+		size   int
+	}
+
+	gzipWriter struct {
+		http.ResponseWriter
+		Writer io.Writer
+	}
+
+	Middleware func(http.Handler, *zap.SugaredLogger) http.Handler
+)
+
+func (w gzipWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
+}
+
+func Conveyor(h http.Handler, sugar *zap.SugaredLogger, middlewares ...Middleware) http.Handler {
+	for _, middleware := range middlewares {
+		h = middleware(h, sugar)
+	}
+	return h
+}
+
+func WriteWithCompression(h http.Handler, sugar *zap.SugaredLogger) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			sugar.Info("Accept-Encoding is not allowed")
+			h.ServeHTTP(w, r)
+			return
+		}
+
+		contentType := r.Header.Get("Content-Type")
+		if contentType != "application/json" && contentType != "text/html" {
+			sugar.Info("Content-Type is not supported for compression. Content-Type: " + contentType)
+			h.ServeHTTP(w, r)
+			return
+		}
+
+		gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
+		if err != nil {
+			sugar.Error("Failed to create gzip writer", zap.Error(err))
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		defer gz.Close()
+
+		w.Header().Set("Content-Encoding", "gzip")
+		h.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
+	})
+}
+
+func ReadWithCompression(h http.Handler, sugar *zap.SugaredLogger) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
+			sugar.Info("Content-Encoding is not allowed")
+			h.ServeHTTP(w, r)
+			return
+		}
+
+		contentType := r.Header.Get("Content-Type")
+		if contentType != "application/json" && contentType != "text/html" {
+			sugar.Info("Content-Type is not supported for decompression. Content-Type: " + contentType)
+			h.ServeHTTP(w, r)
+			return
+		}
+
+		gz, err := gzip.NewReader(r.Body)
+		if err != nil {
+			sugar.Error("Failed to create gzip writer", zap.Error(err))
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		defer gz.Close()
+
+		newReq := r.Clone(r.Context())
+		newReq.Body = gz
+		newReq.ContentLength = -1
+
+		h.ServeHTTP(w, newReq)
+	})
+}
+
+func WithLogging(h http.Handler, sugar *zap.SugaredLogger) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		rd := &responseData{
+			status: 0,
+			size:   0,
+		}
+
+		lw := loggingResponseWriter{
+			ResponseWriter: w,
+			responseData:   rd,
+		}
+		h.ServeHTTP(&lw, r)
+
+		duration := time.Since(start)
+
+		sugar.Infoln(
+			"uri", r.RequestURI,
+			"method", r.Method,
+			"status", rd.status,
+			"duration", duration,
+			"size", rd.size,
+		)
+	})
+}
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode
+}

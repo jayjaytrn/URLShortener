c1074dae44238208b717339bde675dd6ba13ad00
salakhutdinov.m <marat.salakhutdinov@atme.com>
2024-10-25 05:37:00 +0300

    fix

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -192,6 +192,22 @@ func WithAuth(next http.Handler, authManager *auth.Manager, storage db.Shortener
 				http.Error(w, "authorization error", http.StatusInternalServerError)
 				return
 			}
+		}
+		if cookie.Value == "" {
+			logger.Debug("куки есть но пустая")
+			newJWT, err = authManager.BuildJWTStringWithNewID(newUserID)
+			if err != nil {
+				http.Error(w, "authorization error", http.StatusInternalServerError)
+				return
+			}
+			ctx := context.WithValue(r.Context(), "userID", newUserID)
+			r = r.WithContext(ctx)
+			http.SetCookie(w, &http.Cookie{
+				Name:     "Authorization",
+				Value:    newJWT,
+				Path:     "/",
+				HttpOnly: true,
+			})
 		} else {
 			// Если кука существует, проверяем JWT
 			logger.Debug("Кука существует, проверяем JWT")

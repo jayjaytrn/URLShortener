e707d4126b7015d939a9b3654c8147e5f64034f9
salakhutdinov.m <marat.salakhutdinov@atme.com>
2024-10-28 23:23:49 +0300

    fix 401

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -177,9 +177,15 @@ func WithAuth(next http.Handler, authManager *auth.Manager, storage db.Shortener
 		newUserID := storage.GenerateNewUserID()
 		cookie, err := r.Cookie("Authorization")
 		if err != nil {
+			if errors.Is(err, http.ErrNoCookie) {
+				logger.Debug("No cookie: " + err.Error())
+				http.Error(w, "authorization error", http.StatusUnauthorized)
+				return
+			}
+
 			// Если кука отсутствует, создаём новый JWT
 			logger.Debug("Кука отсутствует")
-			if errors.Is(err, http.ErrNoCookie) {
+			if !errors.Is(err, http.ErrNoCookie) {
 				logger.Debug("ErrNoCookie, создаем новый JWT")
 				newJWT, err = authManager.BuildJWTStringWithNewID(newUserID)
 				if err != nil {
@@ -204,11 +210,6 @@ func WithAuth(next http.Handler, authManager *auth.Manager, storage db.Shortener
 			logger.Debug("Кука существует, проверяем JWT")
 			userID, err := authManager.GetUserIdFromJWTString(cookie.Value)
 			if err != nil {
-				if strings.Contains(err.Error(), "token is valid but userID is missing") {
-					logger.Debug("ID не передан: " + err.Error())
-					http.Error(w, "authorization error", http.StatusUnauthorized)
-					return
-				}
 				logger.Debug("Проверить не удалось: " + err.Error())
 				// Если JWT не валиден, создаём новый JWT
 				logger.Debug("Ошибка при получения ID из куки token is not valid: " + err.Error())
